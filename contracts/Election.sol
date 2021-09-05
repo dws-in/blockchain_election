2aaf2288a556cf5cae5bbe4bfe8cf6cef770ece9
dws-in <dwi.wahyu.toso@gmail.com>
2021-09-05 19:19:16 +0700

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Election {
+    string public candidate;
+
+    function Election() public {
+        candidate = "Candidate 1";
+    }
+    //min 24
+}
